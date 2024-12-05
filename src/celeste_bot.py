0fb1c5e556676b97d6209311a8ec35b773f2d6ac
Xaeza Olt <xaezaolt@Xaezas-MacBook-Pro.local>
2024-12-04 21:16:43 -0800

    Added ch anged turnip command to turnip added help

--- src/celeste_bot.py
+++ src/celeste_bot.py
@@ -7,6 +7,11 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+valid_commands = [
+    '!help',
+    '!turnip',
+    ]
+
 @client.event
 async def on_ready():
     print(f'Logged on as {client.user}!')
@@ -17,14 +22,17 @@ async def on_message(message):
         return
 
     # Check if the message starts with the command prefix
-    if message.content.startswith('!price'):
+    if message.content.startswith('!turnip'):
         # Extract the number from the message
         match = re.search(r'\d+', message.content)
         if match:
             number = match.group(0)
-            response = f"{message.author.name}'s Island is currently reporting a turnip price of {number} bells."
+            response = f"I'm not usually one to gossip but I hear @{message.author.name} Island is currently reporting a turnip price of {number} bells."
             await message.channel.send(response)
         else:
             await message.channel.send('Please provide a number after the command.')
+    if message.content.startswith('!help'):
+        response = f"Oh, yes, hello! I respond to: {', '.join(valid_commands)}"
+        await message.channel.send(response)
 
 client.run(os.environ['CELESTE_BOT_TOKEN'])

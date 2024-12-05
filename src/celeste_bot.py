0005a222819177bd45adebe3094c67afca4d7a13
Xaeza Olt <xaezaolt@Xaezas-MacBook-Pro.local>
2024-12-04 22:41:54 -0800

    moved channel ids to a constants.py

--- src/celeste_bot.py
+++ src/celeste_bot.py
@@ -2,9 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import os
-
-TURNIP_CHANNEL_ID = '1313380515671314474'
-METEOR_CHANNEL_ID = '1313371148473470996'
+from constants import TURNIP_CHANNEL_ID, METEOR_CHANNEL_ID
 
 intents = discord.Intents.default()
 intents.message_content = True
